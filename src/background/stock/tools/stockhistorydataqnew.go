df5052afef2ab6ddf8a1592b94a4c292f5318ccf
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-04-04 23:45:35 +0800

    fix

--- src/background/stock/tools/stockhistorydataqnew.go
+++ src/background/stock/tools/stockhistorydataqnew.go
@@ -42,16 +42,19 @@ func main(){
 		return
 	}
 	for _, f := range files {
-		GetHistoryDataQNewFromExcel(f.Name(),db)
+		if err = GetHistoryDataQNewFromExcel(f.Name(),db) ; err != nil{
+			logger.Error(err)
+			return 
+		}
 	}
 }
 
 
-func GetHistoryDataQNewFromExcel(fileName string,db *gorm.DB){
+func GetHistoryDataQNewFromExcel(fileName string,db *gorm.DB)(error){
 	file, err := os.Open(config.GetStorageRoot() + "TransData/HistoryDataNew/" + fileName)
 	if err != nil {
 		logger.Error("File Name : ",fileName, " error : ", err)
-		return
+		return err
 	}
 	defer file.Close()
 
@@ -68,7 +71,7 @@ func GetHistoryDataQNewFromExcel(fileName string,db *gorm.DB){
 			break
 		} else if err != nil {
 			logger.Error("记录集错误:", err)
-			return
+			return err
 		}
 		sql := "insert into stock_history_data_q_new(`code`,`date`,`open`,`high`,`close`,`low`,`volume`,`amount`) select '" + stockCode + "','"
 		for i := 0; i < len(record); i++ {
@@ -79,9 +82,10 @@ func GetHistoryDataQNewFromExcel(fileName string,db *gorm.DB){
 
 		if err = db.Exec(sql).Error ; err != nil{
 			logger.Error(err)
-			return
+			return err
 		}
 	}
+	return nil
 }
 
 
