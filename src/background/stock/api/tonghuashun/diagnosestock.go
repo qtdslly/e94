ce03bd8dfea32656211ec646e1db25fcf0e89516
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-04-10 20:49:19 +0800

    fix

--- src/background/stock/api/tonghuashun/diagnosestock.go
+++ src/background/stock/api/tonghuashun/diagnosestock.go
@@ -129,8 +129,8 @@ func GetControlInfo(code string,db *gorm.DB)(error){
 
 	logger.Print(string(recv))
 	recvTmp := string(recv)
-	recvTmp = strings.Replace(recvTmp,"<span style=\\\"color:green\\\">","",-1)
-	recvTmp = strings.Replace(recvTmp,"</span>","",-1)
+	recvTmp = strings.Replace(recvTmp,"\"<span style=\\\"color:green\\\">","",-1)
+	recvTmp = strings.Replace(recvTmp,"<\\/span>\"","",-1)
 
 	recv = []byte(recvTmp)
 	var control ControlInfo
