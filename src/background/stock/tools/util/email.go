2f8efad2e679c1db5fd3d3c7a2a80bb129443b7d
lyric <liuliyang@lehoolive.com>
2018-04-12 11:36:51 +0800

    fix

--- src/background/stock/tools/util/email.go
+++ src/background/stock/tools/util/email.go
@@ -7,14 +7,14 @@ import (
 
 func SendEmail(subject,content string)(bool){
 	m := gomail.NewMessage()
-	m.SetAddressHeader("From", "947883972@qq.com", "Lyric")  // 发件人
+	m.SetAddressHeader("From", "liuliyang@lehoolive.com", "Lyric")  // 发件人
 	m.SetHeader("To",  // 收件人
 		m.FormatAddress("947883972@qq.com", "Lyric"),
 	)
 	m.SetHeader("Subject", subject)  // 主题
 	m.SetBody("text/html", content)  // 正文
 
-	d := gomail.NewPlainDialer("smtp.qq.com", 25, "947883972@qq.com", "jyrgmylxddaabbhe")  // 发送邮件服务器、端口、发件人账号、发件人密码
+	d := gomail.NewPlainDialer("smtp.ym.163.com", 25, "liuliyang@lehoolive.com", "CWX163821")  // 发送邮件服务器、端口、发件人账号、发件人密码
 	if err := d.DialAndSend(m); err != nil {
 		logger.Error(err)
 		return false
