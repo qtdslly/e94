c960b8d74c7b2949f83623f226c4790b55ef0b69
lyric <liuliyang@lehoolive.com>
2018-04-16 14:44:09 +0800

    fix

--- src/background/stock/tools/profit.go
+++ src/background/stock/tools/profit.go
@@ -55,9 +55,9 @@ func main(){
 		everyProfit = (realTimeStock.NowPrice - realTimeStock.YestdayClosePrice) * float64(holdStock.AllCount)
 		allProfit += everyProfit
 		stockName := util.GetNameByCode(holdStock.Code,db)
-		tishi += stockName + " " + fmt.Sprint(everyProfit) + "\n"
+		tishi += stockName + " " + fmt.Sprint("%.2f",everyProfit) + "\n"
 	}
-	tishi += "all:" + fmt.Sprint(allProfit)
+	tishi += "all:" + fmt.Sprint("%.2f",allProfit)
 
 	logger.Printf(tishi)
 	return
