29acbc214ecb7368d07b2a1ac1688fa2417e8e75
lyric <liuliyang@lehoolive.com>
2018-04-17 09:44:16 +0800

    fix

--- src/background/stock/tools/util/email.go
+++ src/background/stock/tools/util/email.go
@@ -9,12 +9,12 @@ func SendEmail(subject,content string)(bool){
 	m := gomail.NewMessage()
 	m.SetAddressHeader("From", "qtdslly@aliyun.com", "Lyric")  // 发件人
 	m.SetHeader("To",  // 收件人
-		m.FormatAddress("947883972@qq.com", "Lyric"),
+		m.FormatAddress("qtdslly@aliyun.com", "Lyric"),
 	)
 	m.SetHeader("Subject", subject)  // 主题
 	m.SetBody("text/html", content)  // 正文
 
-	d := gomail.NewPlainDialer("smtp.aliyun.com", 465, "qtdslly@aliyun.com", "hacker@lly413")  // 发送邮件服务器、端口、发件人账号、发件人密码
+	d := gomail.NewPlainDialer("smtp.aliyun.com", 465, "qtdslly@aliyun.com", "qtds@lly413")  // 发送邮件服务器、端口、发件人账号、发件人密码
 	if err := d.DialAndSend(m); err != nil {
 		logger.Error(err)
 		return false
