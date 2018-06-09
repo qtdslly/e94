d1205f5a73b83a6bdd537605f5114711d8cea9e2
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-06-09 13:10:10 +0800

    fix

--- src/background/newmovie/model/movie.go
+++ src/background/newmovie/model/movie.go
@@ -2,12 +2,23 @@ package model
 
 import (
 	"github.com/jinzhu/gorm"
+	"time"
 )
 
 type Movie struct {
 	Id                     uint32     `gorm:"primary_key" json:"id"`
 	Title                  string     `json:"title"`
+	Description                  string     `json:"description"`
 	Url                    string    `json:"url"`
+	ThumbX		       string     `json:"thumb_x"`
+	ThumbY		       string     `json:"thumb_y"`
+	Directors		       string     `json:"directors"`
+	Actors		       string     `json:"actors"`
+	Score		       string     `json:"score"`
+	PublishDate            string     `json:"publish_date"`
+
+	CreatedAt       time.Time  `json:"created_at"`
+	UpdatedAt       time.Time  `json:"updated_at"`
 }
 
 func (Movie) TableName() string {
