053f02f30302b2f60a3f50eb197c8a9c817e192b
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-06-09 18:16:14 +0800

    fix

--- src/background/newmovie/controller/newmovie.go
+++ src/background/newmovie/controller/newmovie.go
@@ -10,16 +10,36 @@ import (
 )
 
 func NewMovieListHandler(c *gin.Context) {
+
+	type param struct {
+
+		Limit       uint32 `form:"limit" binding:"required"`
+		Offset      uint32 `form:"offset" binding:"exists"`
+	}
+
+	var p param
+	if err := c.Bind(&p); err != nil {
+		logger.Error(err)
+		return
+	}
+
 	var err error
 
 	db := c.MustGet(constant.ContextDb).(*gorm.DB)
 
 	var movies []model.Movie
-	if err = db.Limit(3).Find(&movies).Error ; err != nil{
+	if err = db.Order("publish_date desc").Offset(p.Offset).Limit(p.Limit).Find(&movies).Error ; err != nil{
 		logger.Error("query movie err!!!,",err)
 		c.AbortWithStatus(http.StatusInternalServerError)
 	}
 
-	c.JSON(http.StatusOK, gin.H{"err_code": constant.Success, "data": movies})
+	var count uint32
+	if err = db.Model(&model.Movie{}).Count(&count).Error; err != nil {
+		logger.Error(err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"err_code": constant.Success, "data": movies,"count":count})
 }
 
