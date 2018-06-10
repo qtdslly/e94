e0131e5d4b4af8815500ae036839029f1deb299b
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-06-10 17:34:38 +0800

    fix

--- src/background/newmovie/controller/cms/setting.go
+++ src/background/newmovie/controller/cms/setting.go
@@ -1,22 +1,16 @@
 package cms
 
 import (
-	"time"
 	"background/common/constant"
 	"background/common/logger"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 
-	"encoding/json"
-	"background/stock/component/kv"
+	"background/newmovie/model"
 )
 
-type ScriptSetting struct {
-	ScriptPeriod int    `json:"script_period"` // s为单位
-	Script       string `json:"script"`
-	UpdatedAt    string `json:"updated_at"`
-}
+
 
 /*
 	POST /cms/setting/script/save
@@ -24,6 +18,11 @@ type ScriptSetting struct {
 	http://localhost:2000/#!./cms/cms-setting.md
 */
 func ScriptSettingSaveHandler(c *gin.Context) {
+
+	type ScriptSetting struct {
+		Script       string `json:"script"`
+	}
+
 	var p ScriptSetting
 	var err error
 
@@ -32,59 +31,44 @@ func ScriptSettingSaveHandler(c *gin.Context) {
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
+
+	logger.Debug(p.Script)
 	db := c.MustGet(constant.ContextDb).(*gorm.DB)
 
-	// 先获取setting
-	set, _ := GetScriptSetting(db)
-	// 当脚本内容不一致时，自动更新时间
-	if set != nil {
-		if set.Script != p.Script {
-			p.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
-		} else {
-			p.UpdatedAt = set.UpdatedAt
+	var kv model.KvStore
+	err = db.Where("`key`= ? and app_id = ? and version_id = ?", constant.ScriptSettingKey, 0, 0).First(&kv).Error;
+	if err == gorm.ErrRecordNotFound {
+		kv.AppId = 0
+		kv.VersionId = 0
+		kv.Key = constant.ScriptSettingKey
+		kv.Value = p.Script
+		if err := db.Create(&kv).Error; err != nil {
+			return err
 		}
 	} else {
-		p.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+		if err := db.Table(model.KvStore{}.TableName()).Where("`key`= ? and app_id = ? and version_id = ?", constant.ScriptSettingKey, 0, 0).Update("value", p.Script).Error; err != nil {
+			return err
+		}
 	}
 
-	if err := SetScriptSetting(&p, db); err != nil {
-		logger.Error(err)
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
+	//// 先获取setting
+	//set, _ := GetScriptSetting(db)
+	//// 当脚本内容不一致时，自动更新时间
+	//if set != nil {
+	//	if set.Script != p.Script {
+	//		p.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+	//	} else {
+	//		p.UpdatedAt = set.UpdatedAt
+	//	}
+	//} else {
+	//	p.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+	//}
+	//
+	//if err := SetScriptSetting(&p, db); err != nil {
+	//	logger.Error(err)
+	//	c.AbortWithStatus(http.StatusInternalServerError)
+	//	return
+	//}
 
 	c.JSON(http.StatusOK, gin.H{"err_code": constant.Success})
-}
-
-
-func GetScriptSetting(db *gorm.DB) (*ScriptSetting, error) {
-	value, err := kv.GetValueForKey(0, 0, constant.ScriptSettingKey, db)
-	if err != nil {
-		return nil, err
-	}
-
-	if value == "" {
-		return nil, kv.ErrKeyNotFound
-	}
-
-	var setting ScriptSetting
-	err = json.Unmarshal([]byte(value), &setting)
-	if err != nil {
-		return nil, err
-	}
-	return &setting, nil
-}
-
-func SetScriptSetting(setting *ScriptSetting, db *gorm.DB) error {
-	value, err := json.Marshal(setting)
-	if err != nil {
-		return err
-	}
-
-	err = kv.SetValueForKey(0, 0, constant.ScriptSettingKey, string(value), db)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
