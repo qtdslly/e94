995132d0e80c61aff728c831a3ec3a64ffeef87e
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-06-10 17:23:54 +0800

    fix

--- src/background/newmovie/controller/api/script.go
+++ src/background/newmovie/controller/api/script.go
@@ -5,7 +5,7 @@ import (
 	"background/common/logger"
 	"background/newmovie/model"
 	"net/http"
-	"time"
+	//"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -28,22 +28,23 @@ func ScriptHandler(c *gin.Context) {
 		return
 	}
 
-	lastUpdate := time.Unix(p.LastUpdate, 0)
+	//lastUpdate := time.Unix(p.LastUpdate, 0)
 
 	db := c.MustGet(constant.ContextDb).(*gorm.DB)
 	var setting model.KvStore
 	if err := db.Where("app_id = 0 and version_id = 0 and `key` = ?",constant.ScriptSettingKey).First(&setting).Error ; err != nil{
 		logger.Error(err)
-		return ""
+		return
 	}
 	// 只有当脚本有内容且更新时间在客户端更新时间之后，才返回脚本内容
-	if setting != nil {
-		updatedAt, _ := time.ParseInLocation("2006-01-02 15:04:05", setting.UpdatedAt, time.Local)
-		if updatedAt.After(lastUpdate) {
-			c.JSON(http.StatusOK, gin.H{"err_code": constant.Success, "data": setting.Value})
-			return
-		}
-	}
+	//if setting != nil {
+	//	updatedAt, _ := time.ParseInLocation("2006-01-02 15:04:05", setting.UpdatedAt, time.Local)
+	//	if updatedAt.After(lastUpdate) {
+	//		c.JSON(http.StatusOK, gin.H{"err_code": constant.Success, "data": setting.Value})
+	//		return
+	//	}
+	//}
+	c.JSON(http.StatusOK, gin.H{"err_code": constant.Success, "data": setting.Value})
 
-	c.JSON(http.StatusOK, gin.H{"err_code": constant.Success})
+	//c.JSON(http.StatusOK, gin.H{"err_code": constant.Success})
 }
