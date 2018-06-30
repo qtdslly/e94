e1ceda72c7132c6c3fb4680a563d3284cbd4e389
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-06-30 11:46:11 +0800

    fix

--- src/background/newmovie/tools/streamthumb.go
+++ src/background/newmovie/tools/streamthumb.go
@@ -57,7 +57,7 @@ func CheckStreamUrl(streamId uint32,url string)bool{
 	c2 := make(chan string, 1)
 	ffmpegAddr := "/usr/bin/ffmpeg"
 	go func() {
-		cmdStr := fmt.Sprintf("%s -i '%s' -y -s 320x240 -vframes 1 /data/www/dreamvideo/public/static/thumb/stream/%d.jpg", ffmpegAddr, url,streamId)
+		cmdStr := fmt.Sprintf("%s -i '%s' -y -s 320x240 -vframes 1 /data/www/dreamvideo/public/thumb/stream/%d.jpg", ffmpegAddr, url,streamId)
 		fmt.Println(cmdStr)
 		cmd := exec.Command("bash", "-c", cmdStr)
 
