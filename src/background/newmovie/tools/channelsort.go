b9db5001173120224344b83a9c3821ff1ec470a5
lyric <liuliyang@lehoolive.com>
2018-07-03 14:41:47 +0800

    fix

--- src/background/newmovie/tools/channelsort.go
+++ src/background/newmovie/tools/channelsort.go
@@ -70,133 +70,135 @@ func main(){
 		}else if strings.Contains(stream.Title,"CCTV"){
 			stream.Sort = 16
 		}else if strings.Contains(stream.Title,"中国教育"){
-			stream.Sort = 17
-		}else if strings.Contains(stream.Title,"CGTN") {
-			stream.Sort = 18
-		}else if strings.Contains(stream.Title,"NewTV") {
-			stream.Sort = 19
-		}else if strings.Contains(stream.Title,"中国黄河") || strings.Contains(stream.Title,"中国气象"){
 			stream.Sort = 20
-		}else if strings.Contains(stream.Title,"湖南卫视"){
-			stream.Sort = 31
-		}else if strings.Contains(stream.Title,"浙江卫视"){
-			stream.Sort = 32
-		}else if strings.Contains(stream.Title,"江苏卫视"){
-			stream.Sort = 33
-		}else if strings.Contains(stream.Title,"东方卫视"){
-			stream.Sort = 34
-		}else if strings.Contains(stream.Title,"北京卫视"){
-			stream.Sort = 35
-		}else if strings.Contains(stream.Title,"卫视") && !strings.Contains(stream.Category,"香港")&& !strings.Contains(stream.Category,"澳门")&& !strings.Contains(stream.Category,"台湾"){
-			stream.Sort = 41
-		}else if strings.Contains(stream.Category,"北京"){
-			stream.Sort = 51
-		}else if strings.Contains(stream.Category,"上海"){
-			stream.Sort = 52
-		}else if strings.Contains(stream.Category,"江苏"){
-			stream.Sort = 53
-		}else if strings.Contains(stream.Category,"浙江"){
-			stream.Sort = 54
-		}else if strings.Contains(stream.Category,"四川"){
-			stream.Sort = 55
-		}else if strings.Contains(stream.Category,"湖北"){
-			stream.Sort = 56
-		}else if strings.Contains(stream.Category,"江西"){
-			stream.Sort = 57
-		}else if strings.Contains(stream.Category,"天津"){
-			stream.Sort = 58
-		}else if strings.Contains(stream.Category,"湖南"){
-			stream.Sort = 59
-		}else if strings.Contains(stream.Category,"河南"){
-			stream.Sort = 60
-		}else if strings.Contains(stream.Category,"广东"){
-			stream.Sort = 61
-		}else if strings.Contains(stream.Category,"安徽"){
-			stream.Sort = 62
-		}else if strings.Contains(stream.Category,"福建"){
-			stream.Sort = 63
-		}else if strings.Contains(stream.Category,"河北"){
-			stream.Sort = 64
-		}else if strings.Contains(stream.Category,"湖南"){
-			stream.Sort = 65
-		}else if strings.Contains(stream.Category,"山东"){
-			stream.Sort = 66
-		}else if strings.Contains(stream.Category,"重庆"){
-			stream.Sort = 67
-		}else if strings.Contains(stream.Category,"陕西"){
-			stream.Sort = 68
-		}else if strings.Contains(stream.Category,"新疆"){
-			stream.Sort = 69
-		}else if strings.Contains(stream.Category,"内蒙古"){
-			stream.Sort = 70
-		}else if strings.Contains(stream.Category,"辽宁"){
-			stream.Sort = 71
-		}else if strings.Contains(stream.Category,"吉林"){
-			stream.Sort = 72
-		}else if strings.Contains(stream.Category,"黑龙江"){
-			stream.Sort = 73
-		}else if strings.Contains(stream.Category,"甘肃"){
-			stream.Sort = 74
-		}else if strings.Contains(stream.Category,"宁夏"){
-			stream.Sort = 75
-		}else if strings.Contains(stream.Category,"贵州"){
-			stream.Sort = 76
-		}else if strings.Contains(stream.Category,"青海"){
-			stream.Sort = 77
-		}else if strings.Contains(stream.Category,"云南"){
-			stream.Sort = 78
-		}else if strings.Contains(stream.Category,"广西"){
-			stream.Sort = 79
-		}else if strings.Contains(stream.Category,"海南"){
+		}else if strings.Contains(stream.Title,"CGTN") {
+			stream.Sort = 30
+		}else if strings.Contains(stream.Title,"NewTV") {
+			stream.Sort = 50
+		}else if strings.Contains(stream.Title,"中国黄河") || strings.Contains(stream.Title,"中国气象"){
 			stream.Sort = 80
+		}else if strings.Contains(stream.Title,"湖南卫视"){
+			stream.Sort = 151
+		}else if strings.Contains(stream.Title,"浙江卫视"){
+			stream.Sort = 152
+		}else if strings.Contains(stream.Title,"江苏卫视"){
+			stream.Sort = 153
+		}else if strings.Contains(stream.Title,"东方卫视"){
+			stream.Sort = 154
+		}else if strings.Contains(stream.Title,"北京卫视"){
+			stream.Sort = 155
+		}else if strings.Contains(stream.Title,"卫视") && !strings.Contains(stream.Category,"香港")&& !strings.Contains(stream.Category,"澳门")&& !strings.Contains(stream.Category,"台湾"){
+			stream.Sort = 200
+		}else if strings.Contains(stream.Category,"北京"){
+			stream.Sort = 300
+		}else if strings.Contains(stream.Category,"上海"){
+			stream.Sort = 400
+		}else if strings.Contains(stream.Category,"江苏"){
+			stream.Sort = 500
+		}else if strings.Contains(stream.Category,"浙江"){
+			stream.Sort = 800
+		}else if strings.Contains(stream.Category,"四川"){
+			stream.Sort = 1100
+		}else if strings.Contains(stream.Category,"湖北"){
+			stream.Sort = 1400
+		}else if strings.Contains(stream.Category,"江西"){
+			stream.Sort = 1700
+		}else if strings.Contains(stream.Category,"天津"){
+			stream.Sort = 2000
+		}else if strings.Contains(stream.Category,"湖南"){
+			stream.Sort = 2300
+		}else if strings.Contains(stream.Category,"河南"){
+			stream.Sort = 2600
+		}else if strings.Contains(stream.Category,"广东"){
+			stream.Sort = 3000
+		}else if strings.Contains(stream.Category,"安徽"){
+			stream.Sort = 3500
+		}else if strings.Contains(stream.Category,"福建"){
+			stream.Sort = 3800
+		}else if strings.Contains(stream.Category,"河北"){
+			stream.Sort = 4100
+		}else if strings.Contains(stream.Category,"湖南"){
+			stream.Sort = 4400
+		}else if strings.Contains(stream.Category,"山东"){
+			stream.Sort = 4700
+		}else if strings.Contains(stream.Category,"重庆"){
+			stream.Sort = 5000
+		}else if strings.Contains(stream.Category,"陕西"){
+			stream.Sort = 5300
+		}else if strings.Contains(stream.Category,"新疆"){
+			stream.Sort = 5600
+		}else if strings.Contains(stream.Category,"内蒙古"){
+			stream.Sort = 5900
+		}else if strings.Contains(stream.Category,"辽宁"){
+			stream.Sort = 6200
+		}else if strings.Contains(stream.Category,"吉林"){
+			stream.Sort = 6500
+		}else if strings.Contains(stream.Category,"黑龙江"){
+			stream.Sort = 6800
+		}else if strings.Contains(stream.Category,"甘肃"){
+			stream.Sort = 7100
+		}else if strings.Contains(stream.Category,"宁夏"){
+			stream.Sort = 7400
+		}else if strings.Contains(stream.Category,"贵州"){
+			stream.Sort = 7700
+		}else if strings.Contains(stream.Category,"青海"){
+			stream.Sort = 8000
+		}else if strings.Contains(stream.Category,"云南"){
+			stream.Sort = 8300
+		}else if strings.Contains(stream.Category,"广西"){
+			stream.Sort = 8600
+		}else if strings.Contains(stream.Category,"海南"){
+			stream.Sort = 8900
 		}else if strings.Contains(stream.Category,"山西"){
-			stream.Sort = 81
+			stream.Sort = 9200
 		}else if strings.Contains(stream.Category,"西藏"){
-			stream.Sort = 82
+			stream.Sort = 9500
 		}else if strings.Contains(stream.Category,"特色") {
-			stream.Sort = 85
+			stream.Sort = 10000
 		}else if strings.Contains(stream.Category,"风景"){
-			stream.Sort = 86
+			stream.Sort = 11000
 		}else if strings.Contains(stream.Category,"香港"){
-			stream.Sort = 91
+			stream.Sort = 13000
 		}else if strings.Contains(stream.Category,"澳门"){
-			stream.Sort = 111
+			stream.Sort = 13300
 		}else if strings.Contains(stream.Category,"台湾"){
-			stream.Sort = 131
+			stream.Sort = 13600
 		}else if strings.Contains(stream.Category,"韩国"){
-			stream.Sort = 161
+			stream.Sort = 14600
 		}else if strings.Contains(stream.Category,"朝鲜"){
-			stream.Sort = 231
+			stream.Sort = 15000
 		}else if strings.Contains(stream.Category,"日本"){
-			stream.Sort = 251
+			stream.Sort = 16000
 		}else if strings.Contains(stream.Category,"新加坡"){
-			stream.Sort = 301
+			stream.Sort = 17000
 		}else if strings.Contains(stream.Category,"泰国"){
-			stream.Sort = 331
+			stream.Sort = 18000
 		}else if strings.Contains(stream.Category,"马来西亚"){
-			stream.Sort = 361
+			stream.Sort = 19000
 		}else if strings.Contains(stream.Category,"美国"){
-			stream.Sort = 401
+			stream.Sort = 20000
+		}else if strings.Contains(stream.Category,"德国") {
+			stream.Sort = 21000
 		}else if strings.Contains(stream.Category,"加拿大"){
-			stream.Sort = 481
+			stream.Sort = 22000
 		}else if strings.Contains(stream.Category,"俄罗斯"){
-			stream.Sort = 521
+			stream.Sort = 23000
 		}else if strings.Contains(stream.Category,"澳大利亚"){
-			stream.Sort = 601
+			stream.Sort = 24000
 		}else if strings.Contains(stream.Category,"沙特阿拉伯"){
-			stream.Sort = 651
+			stream.Sort = 25000
 		}else if strings.Contains(stream.Category,"印度"){
-			stream.Sort = 701
+			stream.Sort = 26000
 		}else if strings.Contains(stream.Category,"英国"){
-			stream.Sort = 751
+			stream.Sort = 27000
 		}else if strings.Contains(stream.Category,"卡塔尔"){
-			stream.Sort = 801
+			stream.Sort = 28000
 		}else if strings.Contains(stream.Category,"西班牙"){
-			stream.Sort = 821
+			stream.Sort = 29000
 		}else if strings.Contains(stream.Category,"欧洲"){
-			stream.Sort = 851
+			stream.Sort = 30000
 		}else if strings.Contains(stream.Category,"其他外国频道"){
-			stream.Sort = 1001
+			stream.Sort = 32000
 		}else{
 			logger.Debug("=================================",stream.Title)
 			stream.Sort = 0
