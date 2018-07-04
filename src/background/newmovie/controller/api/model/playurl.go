c04c642a230ac04e3bd227b7cf8a417b9625b8df
lyric <liuliyang@lehoolive.com>
2018-07-04 10:39:10 +0800

    fix

--- src/background/newmovie/controller/api/model/playurl.go
+++ src/background/newmovie/controller/api/model/playurl.go
@@ -8,7 +8,7 @@ type PlayUrl struct {
 	Id             uint32  `json:"id"`
 	Provider       uint32  `json:"provider"`
 	Url            string  `json:"url"`
-	Quality        uint32  `json:"quality"`
+	Quality        uint8   `json:"quality"`
 	IsPlay         bool    `json:"is_play"`
 }
 
