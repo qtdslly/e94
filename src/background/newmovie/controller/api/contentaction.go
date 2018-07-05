cac52a44c7acc85bd3fbb319fd2c44fae18fb071
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-07-05 22:38:27 +0800

    fix

--- src/background/newmovie/controller/api/contentaction.go
+++ src/background/newmovie/controller/api/contentaction.go
@@ -38,10 +38,10 @@ func DiggListHandler(c *gin.Context) {
 func DiggHandler(c *gin.Context) {
 
 	type param struct {
-		InstallationId   uint64 `json:"content_id" binding:"required"`
+		InstallationId   uint64 `json:"installation_id" binding:"required"`
 		ContentType      uint8  `json:"content_type" binding:"required"`
-		ContentId        uint32 `json:"content_id"`
-		Disable          bool   `json:"disable"`
+		ContentId        uint32 `json:"content_id" binding:"required"`
+		Disable          bool   `json:"disable" binding:"required"`
 	}
 
 	var p param
