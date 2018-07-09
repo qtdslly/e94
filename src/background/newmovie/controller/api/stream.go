cf4dfb211b4d260a43b9fac3ec3cf254a32d6951
lyric <liuliyang@lehoolive.com>
2018-07-09 18:10:30 +0800

    fix

--- src/background/newmovie/controller/api/stream.go
+++ src/background/newmovie/controller/api/stream.go
@@ -189,38 +189,42 @@ func SearchHandler(c *gin.Context) {
 				count++
 			}
 
-			err,title,description,thumb,score,actors,directors,pageUrl := service.GetTencentVideoInfoByTitle(p.Title)
-			if err == nil{
-				var tenVideo ApiStream
-				tenVideo.Title = title
-				tenVideo.Description = description
-				tenVideo.Actors = actors
-				tenVideo.Directors = directors
-				tenVideo.Thumb = thumb
-				tenVideo.PageUrl = pageUrl
-				tenVideo.Score = score
-				tenVideo.Provider = constant.ContentProviderTencent
-				apiModels = append(apiModels, &tenVideo)
-				count++
+			if count == 0{
+				err,title,description,thumb,score,actors,directors,pageUrl := service.GetTencentVideoInfoByTitle(p.Title)
+				if err == nil{
+					var tenVideo ApiStream
+					tenVideo.Title = title
+					tenVideo.Description = description
+					tenVideo.Actors = actors
+					tenVideo.Directors = directors
+					tenVideo.Thumb = thumb
+					tenVideo.PageUrl = pageUrl
+					tenVideo.Score = score
+					tenVideo.Provider = constant.ContentProviderTencent
+					apiModels = append(apiModels, &tenVideo)
+					count++
+				}
 			}
 
-			err,title,score,area,description,actors,directors,thumb,pageUrl,publishDate := service.GetIqiyiVideoInfoByTitle(p.Title)
-			if err == nil{
-				var iqiyiVideo ApiStream
-				iqiyiVideo.Title = title
-				iqiyiVideo.Description = description
-				iqiyiVideo.Actors = actors
-				iqiyiVideo.Directors = directors
-				iqiyiVideo.Thumb = thumb
-				iqiyiVideo.PageUrl = pageUrl
-				iqiyiVideo.Score = score
-				iqiyiVideo.Area = area
-				iqiyiVideo.Provider = constant.ContentProviderIqiyi
-				apiModels = append(apiModels, &iqiyiVideo)
-				count++
+			if count == 0{
+				err,title,score,area,description,actors,directors,thumb,pageUrl,publishDate := service.GetIqiyiVideoInfoByTitle(p.Title)
+				if err == nil{
+					var iqiyiVideo ApiStream
+					iqiyiVideo.Title = title
+					iqiyiVideo.Description = description
+					iqiyiVideo.Actors = actors
+					iqiyiVideo.Directors = directors
+					iqiyiVideo.Thumb = thumb
+					iqiyiVideo.PageUrl = pageUrl
+					iqiyiVideo.Score = score
+					iqiyiVideo.Area = area
+					iqiyiVideo.Provider = constant.ContentProviderIqiyi
+					apiModels = append(apiModels, &iqiyiVideo)
+					count++
+				}
 			}
 		}
-		
+
 		var videos []model.Video
 		if err = db.Offset(p.Offset).Limit(p.Limit - count).Order("sort asc").Where("title like ? and on_line = ?", "%" + p.Title + "%", constant.MediaStatusOnLine).Find(&videos).Error; err != nil {
 			logger.Error("query video err!!!,", err)
