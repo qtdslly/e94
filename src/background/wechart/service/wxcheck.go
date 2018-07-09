248e0421757a2aeca50801ed2bd0ca60c1c29119
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-07-09 23:02:17 +0800

    fix

--- src/background/wechart/service/wxcheck.go
+++ src/background/wechart/service/wxcheck.go
@@ -5,6 +5,10 @@ import (
 	"sort"
 	"crypto/sha1"
 	"encoding/hex"
+	"net/http"
+	"github.com/tidwall/gjson"
+	"background/common/logger"
+	"io/ioutil"
 )
 
 func Check(timeStamp int64,nonce,signature string)bool{
@@ -19,4 +23,35 @@ func Check(timeStamp int64,nonce,signature string)bool{
 		return true
 	}
 	return false
+}
+
+
+func AccessToken()string{
+	apiUrl := "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx508e9e50a737c414&secret=wx508e9e50a737c414"
+
+	requ, err := http.NewRequest("GET",apiUrl,nil)
+
+	resp, err := http.DefaultClient.Do(requ)
+	if err != nil {
+		logger.Debug("Proxy failed!")
+		return
+	}
+
+	recv,err := ioutil.ReadAll(resp.Body)
+	if err != nil{
+		logger.Error(err)
+		return ""
+	}
+
+	logger.Debug(string(recv))
+	accessToken := gjson.Get(string(recv), "access_token")
+
+	if !accessToken.Exists(){
+		errCode := gjson.Get(string(recv), "errcode")
+		errMsg := gjson.Get(string(recv), "errmsg")
+		logger.Debug(errCode)
+		logger.Debug(errMsg)
+		return ""
+	}
+	return accessToken.String()
 }
