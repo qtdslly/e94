7a8f09a392cb5a5f71d253f0246aaaffc858a453
lyric <liuliyang@lehoolive.com>
2018-07-10 11:12:14 +0800

    fix

--- src/background/wechart/service/wx.go
+++ src/background/wechart/service/wx.go
@@ -12,20 +12,42 @@ func SearchVideo(title string)(*apimodel.Video){
 
 	var youkuVideo apimodel.Video
 
-	err,title,description,actors,directors,thumb,pageUrl,publishDate := GetYoukuVideoInfoByTitle(title)
-	if err == nil{
-		youkuVideo.Title = title
-		youkuVideo.Description = description
-		youkuVideo.Actors = actors
-		youkuVideo.Directors = directors
-		youkuVideo.ThumbY = thumb
-		youkuVideo.PageUrl = pageUrl
-		youkuVideo.PublishDate = publishDate
-		youkuVideo.ContentType = constant.MediaTypeEpisode
-		youkuVideo.Provider = constant.ContentProviderYouKu
-		count++
-	}else{
-		logger.Debug(err)
+	if count == 0{
+		err,title,score,area,description,actors,directors,thumb,pageUrl,publishDate := GetIqiyiVideoInfoByTitle(title)
+		if err == nil{
+			youkuVideo.Title = title
+			youkuVideo.Description = description
+			youkuVideo.Actors = actors
+			youkuVideo.Directors = directors
+			youkuVideo.ThumbY = thumb
+			youkuVideo.PageUrl = pageUrl
+			youkuVideo.Score = score
+			youkuVideo.Country = area
+			youkuVideo.ContentType = constant.MediaTypeEpisode
+			youkuVideo.PublishDate = publishDate
+			youkuVideo.Provider = constant.ContentProviderIqiyi
+			count++
+		}else{
+			logger.Debug(err)
+		}
+	}
+
+	if count == 0{
+		err,title,description,actors,directors,thumb,pageUrl,publishDate := GetYoukuVideoInfoByTitle(title)
+		if err == nil{
+			youkuVideo.Title = title
+			youkuVideo.Description = description
+			youkuVideo.Actors = actors
+			youkuVideo.Directors = directors
+			youkuVideo.ThumbY = thumb
+			youkuVideo.PageUrl = pageUrl
+			youkuVideo.PublishDate = publishDate
+			youkuVideo.ContentType = constant.MediaTypeEpisode
+			youkuVideo.Provider = constant.ContentProviderYouKu
+			count++
+		}else{
+			logger.Debug(err)
+		}
 	}
 
 	if count == 0{
@@ -46,25 +68,7 @@ func SearchVideo(title string)(*apimodel.Video){
 		}
 	}
 
-	if count == 0{
-		err,title,score,area,description,actors,directors,thumb,pageUrl,publishDate := GetIqiyiVideoInfoByTitle(title)
-		if err == nil{
-			youkuVideo.Title = title
-			youkuVideo.Description = description
-			youkuVideo.Actors = actors
-			youkuVideo.Directors = directors
-			youkuVideo.ThumbY = thumb
-			youkuVideo.PageUrl = pageUrl
-			youkuVideo.Score = score
-			youkuVideo.Country = area
-			youkuVideo.ContentType = constant.MediaTypeEpisode
-			youkuVideo.PublishDate = publishDate
-			youkuVideo.Provider = constant.ContentProviderIqiyi
-			count++
-		}else{
-			logger.Debug(err)
-		}
-	}
+
 
 
 	return &youkuVideo
