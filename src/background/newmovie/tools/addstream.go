c9527f54d662e19862789e61b09a8c642b77c360
lyric <liuliyang@lehoolive.com>
2018-07-18 14:26:07 +0800

    fix

--- src/background/newmovie/tools/addstream.go
+++ src/background/newmovie/tools/addstream.go
@@ -3,15 +3,10 @@ package main
 import (
 	"background/newmovie/config"
 	"background/common/logger"
-	"background/newmovie/model"
-	"background/common/util"
-
-	"strings"
-	"background/common/constant"
+	"flag"
+	"background/newmovie/util"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-
-	"flag"
 )
 
 func main(){
@@ -26,7 +21,7 @@ func main(){
 
 
 	if len(*title) == 0 || len(*url) == 0{
-		logger.Debug("useage: ./addstream -p 8 -t 北京卫视 -u http://www.abc.m3u8")
+		logger.Debug("useage: ./addstream -t 北京卫视 -u http://www.abc.m3u8")
 		return
 	}
 	err := config.LoadConfig(*configPath)
@@ -41,97 +36,7 @@ func main(){
 	}
 	db.LogMode(true)
 
-	tx := db.Begin()
-
-	u := *url
-	start := strings.Index(u,"m3u8")
-	m3u8 := ""
-	if start >= 0{
-		m3u8 = u[start:start+4]
-	}
-	var sort uint32
-	if m3u8 == "m3u8"{
-		sort = uint32(1)
-	}else{
-		sort = uint32(10)
-	}
-
-	urlFound := false
-	var play model.PlayUrl
-	play.Url = *url
-	if err := tx.Where("content_type = 4 and url = ?",play.Url).First(&play).Error ; err == gorm.ErrRecordNotFound{
-	}else if err == nil{
-		urlFound = true
-	}
-
-	sTitle := *title
-	sTitle = strings.Replace(sTitle,"高清","",-1)
-	sTitle = strings.Replace(sTitle,"-","",-1)
-	sTitle = util.TrimChinese(sTitle)
-	sTitle = strings.Trim(sTitle," ")
-	if urlFound {
-		var stream model.Stream
-		if err := tx.Where("id = ?", play.ContentId).First(&stream).Error; err != nil {
-			tx.Rollback()
-			logger.Error(err)
-			return
-		}
-
-		stream.Sort = sort
-		stream.Title = sTitle
-		stream.Pinyin = util.TitleToPinyin(stream.Title)
-		logger.Debug(stream.Title)
-		if err = tx.Save(&stream).Error ; err != nil{
-			logger.Error(err)
-			tx.Rollback()
-			return
-		}
-		play.Sort = sort
-		play.Title = *title
-		if err = tx.Save(&play).Error ; err != nil{
-			logger.Error(err)
-			tx.Rollback()
-			return
-		}
-	}else{
-		var stream model.Stream
-		stream.Title = sTitle
-		if err := tx.Where("title = ?",stream.Title).First(&stream).Error ; err == gorm.ErrRecordNotFound{
-			if strings.Contains(stream.Title,"CCTV"){
-				stream.Category = "央视"
-			}else if strings.Contains(stream.Title,"卫视"){
-				stream.Category = "卫视"
-			}else{
-				stream.Category = *category
-			}
-			stream.Pinyin = util.TitleToPinyin(stream.Title)
-
-			stream.OnLine = constant.MediaStatusOnLine
-			stream.Sort = sort
-
-			if err = tx.Create(&stream).Error ; err != nil{
-				tx.Rollback()
-				logger.Error(err)
-				return
-			}
-		}
-
-		play.Url = *url
-		play.ContentId = stream.Id
-		play.Provider = uint32(0)
-		play.Title = *title
-		play.OnLine = constant.MediaStatusOnLine
-		play.Sort = sort
-		play.ContentType = uint8(constant.MediaTypeStream)
-		play.Quality = uint8(constant.VideoQuality720p)
-		if err = tx.Create(&play).Error ; err != nil{
-			tx.Rollback()
-			logger.Error(err)
-			return
-		}
-	}
-
-	tx.Commit()
+	util.StreamAdd(*title,*url,*category,db)
 }
 
 
--- src/background/newmovie/tools/addstreams.go
+++ src/background/newmovie/tools/addstreams.go
@@ -4,10 +4,9 @@ import (
 	"background/newmovie/config"
 	"background/common/logger"
 	"background/newmovie/model"
-	"background/common/util"
 
 	"strings"
-	"background/common/constant"
+	"background/newmovie/util"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 
@@ -21,7 +20,14 @@ func main(){
 	logger.SetLevel(config.GetLoggerLevel())
 
 	configPath := flag.String("conf", "../config/config.json", "Config file path")
+	file := flag.String("file", "list.txt", "file name")
+
 	flag.Parse()
+
+	if len(*file) == 0{
+		logger.Debug("useage: ./addstreams -file list.txt")
+		return
+	}
 
 	err := config.LoadConfig(*configPath)
 	if err != nil {
@@ -35,7 +41,7 @@ func main(){
 	}
 	db.LogMode(true)
 	model.InitModel(db)
-	f, err := os.Open("/home/lyric/Git/e94/src/background/newmovie/tools/result.txt")
+	f, err := os.Open(*file)
 	if err != nil {
 		logger.Error(err)
 	}
@@ -53,61 +59,20 @@ func main(){
 
 		var title string
 		var url string
+		var category string
 		fields := strings.Split(line, "|")
 		if len(fields) == 2 {
 			title = fields[0]
 			url = fields[1]
-		} else {
+		} else if len(fields) == 3 {
+			title = fields[0]
+			url = fields[1]
+			category = fields[2]
+		}else {
 			continue
 		}
 
-		var stream model.Stream
-		stream.Title = title
-		stream.Title = strings.Replace(stream.Title,"高清","",-1)
-		stream.Title = strings.Replace(stream.Title,"-","",-1)
-		//stream.Title = util.TrimChinese(stream.Title)
-		stream.Pinyin = util.TitleToPinyin(stream.Title)
-		stream.Title = strings.Trim(stream.Title," ")
-		logger.Debug(stream.Title)
-
-		tx := db.Begin()
-		if err := tx.Where("title = ?",stream.Title).First(&stream).Error ; err == gorm.ErrRecordNotFound{
-			if strings.Contains(stream.Title,"CCTV"){
-				stream.Category = "央视"
-			}else if strings.Contains(stream.Title,"卫视"){
-				stream.Category = "卫视"
-			}else{
-				stream.Category = "地方"
-			}
-
-			stream.OnLine = constant.MediaStatusOnLine
-			stream.Sort = 0
-
-			if err = tx.Create(&stream).Error ; err != nil{
-				tx.Rollback()
-				logger.Error(err)
-				return
-			}
-		}
-
-		var play model.PlayUrl
-		play.Url = url
-		play.Provider = uint32(constant.ContentProviderSystem)
-		if err := tx.Where("provider = ? and url = ?",play.Provider,play.Url).First(&play).Error ; err == gorm.ErrRecordNotFound{
-			play.Title = title
-			play.OnLine = constant.MediaStatusOnLine
-			play.Sort = 0
-			play.ContentType = uint8(constant.MediaTypeStream)
-			play.ContentId = stream.Id
-			play.Quality = uint8(constant.VideoQuality720p)
-
-			if err = tx.Create(&play).Error ; err != nil{
-				tx.Rollback()
-				logger.Error(err)
-				return
-			}
-		}
-		tx.Commit()
+		util.StreamAdd(title,url,category,db)
 	}
 
 
