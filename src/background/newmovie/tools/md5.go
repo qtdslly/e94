decbfa47a83a9745b6759f0d977e736d4665866a
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-07-20 23:34:12 +0800

    fix

--- src/background/newmovie/tools/md5.go
+++ src/background/newmovie/tools/md5.go
@@ -16,9 +16,10 @@ func main() {
 	if inerr == nil {
 		md5h := md5.New()
 		io.Copy(md5h, file)
-		fmt.Println("%x", md5h.Sum([]byte(""))) //md5
+		fmt.Printf("%x", md5h.Sum([]byte(""))) //md5
 	}else{
 		fmt.Println(inerr)
 	}
 }
 //67 174 86 56 168 141 206 30 171 164 65 219 90 107 53 124
+//241 201 139 185 162 92 212 54 166 225 26 106 69 38 59 135
