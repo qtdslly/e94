ec2c36e0a0fbbda775f0adba293e4a7509ba571a
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-07-20 23:06:03 +0800

    fix

---
+++ src/background/newmovie/tools/md5.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+
+func main() {
+	testFile := "t.txt"
+	log.Println(testFile)
+	file, inerr := os.Open(testFile)
+	if inerr == nil {
+		md5h := md5.New()
+		io.Copy(md5h, file)
+		fmt.Printf("%x", md5h.Sum([]byte(""))) //md5
+	}else{
+		fmt.Println(inerr)
+	}
+}
