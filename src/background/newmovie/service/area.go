d49397aaa965ac7686da3d6a0b7046da0e7e1b31
qtdslly <34677211+qtdslly@users.noreply.github.com>
2018-07-21 17:30:35 +0800

    fix

--- src/background/newmovie/service/area.go
+++ src/background/newmovie/service/area.go
@@ -16,7 +16,7 @@ func SetArea(file string){
 		logger.Error(err)
 	}
 	defer f.Close()
-	
+
 	rd := bufio.NewReader(f)
 	for {
 		line, err := rd.ReadString('\n')
@@ -28,7 +28,9 @@ func SetArea(file string){
 		fields := strings.Split(line, "|")
 		area := fields[1]
 		area = strings.Replace(area,"区","",-1)
-		area = strings.Replace(area,"县","",-1)
+		if len(area) > 9{
+			area = strings.Replace(area,"县","",-1)
+		}
 		area = strings.Replace(area,"自治","",-1)
 		area = strings.Replace(area,"市","",-1)
 		area = strings.Replace(area,"前旗","",-1)
@@ -36,6 +38,9 @@ func SetArea(file string){
 		area = strings.Replace(area,"后旗","",-1)
 		area = strings.Replace(area,"旗","",-1)
 		area = strings.Replace(area,"州","",-1)
+		if fields[1] == "海州区"{
+			area = "海州"
+		}
 		if area == ""{
 			continue
 		}
